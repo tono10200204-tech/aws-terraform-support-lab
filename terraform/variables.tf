ae4a89de84c2a18bbb4ea5a6b59c9fb4c96946a7
Tono <t.ono10200204@gmail.com>
2026-09-07 13:41:19 +0900

    Initial Terraform VPC infrastructure

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "aws-terraform-support-lab"
+}
